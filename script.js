180704b9684d8465a5323dd99ec57df3275f5902
Kabouri Mouad <Kabmouad274@gmail>
2025-07-26 00:36:15 +0200

    Made some inhancements

--- script.js
+++ script.js
@@ -126,4 +126,18 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   `;
   document.head.appendChild(style);
+  const backToTop = document.getElementById('backToTop');
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTop.style.display = "block";
+    } else {
+    backToTop.style.display = "none";
+    }
+  };
+  backToTop.addEventListener('click', function () {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+  });
 });
